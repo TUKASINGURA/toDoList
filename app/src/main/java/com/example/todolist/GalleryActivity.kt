31e62f04e5e9dd5f94ad8f9a00a2a0b2932cbecb
tukasingura <tukasingurarichard551@gmail.com>
2023-08-31 10:32:39 +0300

    fifth commin on the toDoApp

--- app/src/main/java/com/example/todolist/GalleryActivity.kt
+++ app/src/main/java/com/example/todolist/GalleryActivity.kt
@@ -3,14 +3,23 @@ package com.example.todolist
 import android.annotation.SuppressLint
 import android.app.DatePickerDialog
 import android.content.ActivityNotFoundException
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.hardware.camera2.CameraCaptureSession
+import android.hardware.camera2.CameraDevice
+import android.hardware.camera2.CameraManager
+import android.hardware.camera2.CaptureResult
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
 import android.provider.MediaStore
 import android.util.Log
 import android.widget.DatePicker
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.getSystemService
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Locale
@@ -19,20 +28,40 @@ class GalleryActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener{
 
     private val calender= Calendar.getInstance()
     private val formatter= SimpleDateFormat("MMM,dd, YYYY", Locale.US)
-
+    lateinit var handler:Handler
+    lateinit var handlerThread: Thread
+    lateinit var cameraManager: CameraManager
+    lateinit var textView: TextView
+    lateinit var cameraCaptureSession: CameraCaptureSession
+    lateinit var cameraDevice: CameraDevice
+    lateinit var captureResult: CaptureResult
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gallery)
+        get_permission()
 
-        //start of the activity for the camera action
-        val imageCapture = findViewById<ImageView>(R.id.capture_image)
+        var imageCapture = findViewById<ImageView>(R.id.capture_image)
+        cameraManager=getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        handlerThread= HandlerThread("videoThread")
+        handlerThread.start()
+        handler= Handler((handlerThread as HandlerThread).looper)
+
         imageCapture.setOnClickListener{
-            dispatchTakePictureIntent()
-            }
 
-        
-///end of activity for the camera action on the application
+        }
+
+//        //start of the activity for the camera action
+//        val imageCapture = findViewById<ImageView>(R.id.capture_image)
+//        imageCapture.setOnClickListener{
+//            dispatchTakePictureIntent()
+//
+//
+//            }
+//
+//
+/////end of activity for the camera action on the application
+//
 
         val datePicker= findViewById<TextView>(R.id.calender)
         datePicker.setOnClickListener{
@@ -45,16 +74,16 @@ class GalleryActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener{
             ) .show()
         }
     }
-    val REQUEST_IMAGE_CAPTURE = 1
-
-    private fun dispatchTakePictureIntent() {
-        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        try {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
-        } catch (e: ActivityNotFoundException) {
-            // display error state to the user
-        }
-    }
+//    val REQUEST_IMAGE_CAPTURE = 1
+//
+//    private fun dispatchTakePictureIntent() {
+//        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+//        try {
+//            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+//        } catch (e: ActivityNotFoundException) {
+//            // display error state to the user
+//        }
+//    }
 
     override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayofMonth: Int) {
         Log.e("calender", "$year==$month==$dayofMonth")
@@ -65,4 +94,30 @@ class GalleryActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener{
             findViewById<TextView>(R.id.calender).text= formatter.format(timestamp)
             Log.i("formatting",timestamp.toString())
         }
+    //function for implemantation of the camera
+
+    fun get_permission(){
+        var permissionsLists= mutableListOf<String>()
+
+        if(checkSelfPermission(android.Manifest.permission.CAMERA)!=PackageManager.PERMISSION_GRANTED)permissionsLists.add(android.Manifest.permission.CAMERA)
+        if(checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE)!=PackageManager.PERMISSION_GRANTED)permissionsLists.add(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+        if(checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)!=PackageManager.PERMISSION_GRANTED)permissionsLists.add(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
+        if (permissionsLists.size>0){
+            requestPermissions(permissionsLists.toTypedArray(),101)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        grantResults.forEach {
+            if (it!=PackageManager.PERMISSION_GRANTED)
+                get_permission()
+        }
+    }
+    // the function for implementation of the camera ends here
 }
